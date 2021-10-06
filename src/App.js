45bc1d999c0b45af50f03e0be30149a2018dbd44
Thyerre <thyerreesbaltar@gmail.com>
2021-10-06 15:14:44 -0300

    Inserido o conteudo feito nas video aulas API2, Estrutura Inicial e Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import './App.css';
+import { Api } from './api/Api'
+import { Header } from "./Components/Header";
+import { Home } from "./Components/Home";
+import { Footer } from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+
+        <Header />
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
 
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
+import {ReactComponent as Dogs} from '../Assets/dogs.svg'
+
+export const Header = () => {
+    return (
+        <header className={styles.header}>
+            <nav className={`${styles.nav} container`}>
+                <Link className={styles.logo} to="/" aria-label="Dogs - Home"><Dogs/></Link>
+                <Link className={styles.login} to="/login">Login / Criar</Link>
+
+            </nav>
+        </header>
+    )
+}
